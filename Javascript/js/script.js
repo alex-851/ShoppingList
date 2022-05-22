a5ea1ad59db9fd36208d557b9363feedae1ef3b2
alex-851 <99877694+alex-851@users.noreply.github.com>
2022-05-22 17:39:54 +0530

    Update script.js

--- Javascript/js/script.js
+++ Javascript/js/script.js
@@ -1,58 +1,50 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
-var listofli=document.querySelectorAll("li");
-var deletebutton=document.createElement("button");
-var body=document.body;
-body.style.backgroundColor="GREEN";
+var listofli = document.querySelectorAll("li");
+var deletebutton = document.createElement("button");
+var body = document.body;
+body.style.backgroundColor = "GREEN";
 deletebutton.appendChild(document.createTextNode("deletbtn"));
 
 
-for (let index = 0; index < listofli.length; index++) {
-    listofli[index].classList.add("done");
-    
-    
-   
-    
-}
 
-function checkLis(){
+function checkLis() {
     return document.getElementsByTagName('li').length;
-  }
+}
 
 
 function inputLength() {
-	return input.value.length;
+    return input.value.length;
 }
 
 function createListElement() {
-    var deletebutton=document.createElement("button");
+    var deletebutton = document.createElement("button");
     deletebutton.appendChild(document.createTextNode("remove"));
-	var li = document.createElement("li");
+    var li = document.createElement("li");
 
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
-    if(checkLis()>0)
-    {
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+    if (checkLis() > 0) {
         var emptyList = document.querySelector('h2');
-            emptyList.style.display = "none";
+        emptyList.style.display = "none";
 
     }
-    li.addEventListener("click",toggleclass);
-    li.insertAdjacentElement("beforeend",deletebutton);//or li.append(deletebutton); 
-    deletebutton.addEventListener("click",dltitem);
-    
+    li.addEventListener("click", toggleclass);
+    li.insertAdjacentElement("beforeend", deletebutton);//or li.append(deletebutton); 
+    deletebutton.addEventListener("click", dltitem);
+
 
     function dltitem() {
-        
+
         deletebutton.parentElement.remove();
-        if(checkLis() == 0) {
+        if (checkLis() == 0) {
             var emptyList = document.querySelector('h2');
             emptyList.style.display = "block";
-          }
-        
-        
+        }
+
+
     }
 }
 
@@ -60,48 +52,30 @@ function createListElement() {
 
 function addListAfterClick() {
 
- 
-   
-	if (inputLength() > 0) {
-		createListElement();
-	}
+
+
+    if (inputLength() > 0) {
+        createListElement();
+    }
 }
 
 function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
+    if (inputLength() > 0 && event.keyCode === 13) {
+        createListElement();
+    }
 }
 
 function toggleclass() {
-    
+
     this.classList.toggle("done");
-    if(this.className==="done"){
-        this.style.textDecoration="line-through";
+    if (this.className === "done") {
+        this.style.textDecoration = "line-through";
     }
     else
-    this.style.textDecoration="none";
+        this.style.textDecoration = "none";
 }
 
 
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
-
-
-
-
-
-// console.log("hi");
-// var username="ankit";
-// var password=156;
-// var signin=function(user,pass){
-//     if(user===username&&pass===password)
-//     {
-//         console.log("sign sucess");
-//     }
-//     else
-//     console.log("not sucess");
-// };
-
-// signin("ankit",156);
